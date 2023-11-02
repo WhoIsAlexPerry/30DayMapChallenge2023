8fd2c45f37c7dfda80e798eae2b933743d344444
WhoIsAlexPerry <alexperry20@u.northwestern.edu>
2023-11-02 08:33:37 -0500

    changed the name of the dataset

--- 1_Points.R
+++ 1_Points.R
@@ -3,18 +3,15 @@ library(maps)
 library(ggplot2)
 
 #data 
-reutersofficesglobal <- read.csv("data/Alloffices.csv")
+reutersofficesglobal <- read.csv("data/reutersoffices.csv")
 #making world map
 world_map <- map_data("world")
 
 #Base map
 basemap <- ggplot() + coord_fixed() +
-  xlab("") + ylab("") + geom_polygon(data=world_map, aes(x=long, y=lat, group=group), 
+  xlab("") + ylab("") + geom_polygon(data=world_map, aes(x=long, y=lat, group = group), 
                                        colour="black", fill="black") +
-  theme_minimal() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-                          panel.background = element_rect(fill = 'white', colour = 'white'), 
-                          axis.line = element_line(colour = "white"),
-                          axis.ticks=element_blank(), axis.text.x=element_blank(),
+  theme_minimal() + theme(panel.background = element_rect(fill = 'white', colour = 'white'), axis.text.x=element_blank(),
                           axis.text.y=element_blank())
 
 #Add simple data points and title to map
@@ -22,10 +19,13 @@ locations_map <-
   basemap +
   geom_point(data=reutersofficesglobal, 
              aes(x=longitude, y=latitude), colour="Orange", 
-             pch=16, size=2.8, alpha=I(0.7)) +
-  labs(title = "Reuters' Global Office Locations", subtitle = "30 Day Map Challenge | Day One | Points", 
-       caption = "Source:https://www.thomsonreuters.com/en/locations.html",
-plot.subtitle = element_text(hjust = 0), plot.caption = element_text(hjust = -10))
+             pch=20, size=2.8, alpha=(0.7)) +
+  labs(title = "Reuters' Global Office Locations", 
+       subtitle = "30 Day Map Challenge | Day One | Points", 
+       caption = "Source: https://www.thomsonreuters.com/en/locations.html",
+plot.subtitle = element_text(hjust = 0)) +
+  theme(plot.caption = element_text(hjust = 0, vjust = 14,
+                                    size = 10))
 
 #Credits
 #https://sarahleejane.github.io/learning/r/2014/09/21/plotting-data-points-on-maps-with-r.html
